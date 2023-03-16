95c0c0e78d174c3bcfee12bcb00b5e777fb0ea29
Viktoria <vik25@i.ua>
2023-03-16 18:14:08 +0100

    add fatch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const API_KEY = '33139428-a4880fd896903b0937526f617';
+
+export class App extends Component {
+  state = {
+    images: [],
+    isLoading: false,
+    error: null,
+  };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    );
+    this.setState({ isLoading: true });
+
+    try {
+      const images = response.data.hits;
+      this.setState({ images });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    const imageList = this.state.images;
+    return (
+      <div>
+        <div>
+          <input></input>
+        </div>
+        <ul>
+          {imageList.map(({ id, webformatURL, largeImageURL }) => (
+            <li key={id}>
+              <img src={webformatURL} alt="image" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
